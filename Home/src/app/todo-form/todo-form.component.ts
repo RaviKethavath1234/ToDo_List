ef4aab68755c237a3dffa13aaabec55d3f3dd048
RaviKethavath1234 <ravikethavath70@gmail.com>
2025-02-02 11:46:24 +0530

    ToDo_List Project is Done, Date 02-01-2025

--- Home/src/app/todo-form/todo-form.component.ts
+++ Home/src/app/todo-form/todo-form.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  standalone: true,  // Indicate that this is a standalone component
+  imports: [CommonModule, FormsModule], // Import necessary modules here
   templateUrl: './todo-form.component.html',
-  styleUrl: './todo-form.component.css'
+  styleUrls: ['./todo-form.component.css']
 })
 export class TodoFormComponent {
+  inputText: string = '';
+  constructor(public todoService:TodoService){}
+    
+  addForm() {
+    this.todoService.addToDo(this.inputText)
+    this.inputText="";
+  }
+  
 
+  resetForm() {
+    this.inputText = '';
+  }
 }
--- Home/src/app/todo.service.ts
+++ Home/src/app/todo.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TodoService {
-
+taskID=4;
   constructor() { }
+  todolist = [
+    { id: 1, Task: "Task1" },
+    { id: 2, Task: "Task2" },
+    { id: 3, Task: "Task3" }
+  ];
+
+  addToDo(TaskName:any){
+    this.todolist.push({"id":this.taskID++,"Task":TaskName});
+  }
+
+  removeIndex(index:any){
+    this.todolist.splice(index);
+  }
 }
